15c0202860925aa2bfee3f0e02d5aa965898186d
MaverickShivam <shivam.te18@bmsce.ac.in>
2021-07-19 11:59:01 +0530

    completed design and UI

--- src/App.js
+++ src/App.js
@@ -4,20 +4,63 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className="emi-input">
+          <div className="heading">Loan EMI Calculator</div>
+          <div className="form">
+            <table>
+              <tr>
+                <td>Loan Amount</td>
+                <td><input/></td>
+                <td>INR</td>
+              </tr>
+
+              <tr>
+                <td>Loan Tenure</td>
+                <td><input/></td>
+                <td>Months</td>
+              </tr>
+
+              <tr>
+                <td>Interest Rate</td>
+                <td><input/></td>
+                <td>%</td>
+              </tr>
+            </table>
+          </div> 
+          <div className="my-button">
+              CALCULATE
+          </div>
+        </div>
+
+
+        <div className="emi-output">
+          
+          <div className="small-heading">Loan EMI Calculator</div>
+          
+          <div className="first-row">
+            <div className="emi">
+              <div>
+                Loan EMI<br/> ₹ 52,668
+              </div>
+            </div>
+
+            <div className="interest">
+              <div>
+                Total Interest Payable<br/> ₹ 52,668
+              </div>
+            </div>
+          </div>
+
+          <div className="second-row">
+            <div>
+            Total Payment (Principal + Interest)<br/> ₹ 52,668
+            </div>
+          </div>
+
+        </div>
+
+      </div>
     </div>
   );
 }
